5d37e153bca8bae880df540d318d22dac8be5822
Vimoksh Bhavsar <85449611+tankotkirn@users.noreply.github.com>
2023-10-12 12:05:44 +0530

    Added functionalities for Multiple file formats

--- testing/client.java
+++ testing/client.java
@@ -8,25 +8,24 @@ public class client {
         
         try {
             Socket socket = new Socket(SERVER_IP, SERVER_PORT);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
 
             // Input the file name to request
             System.out.print("Enter the file name to request: ");
             String fileName = new BufferedReader(new InputStreamReader(System.in)).readLine();
 
             // Send the requested file name to the server
-            writer.write(fileName + "\n");
-            writer.flush();
+            dataOutputStream.writeUTF(fileName);
 
-            String response = reader.readLine();
+            String response = dataInputStream.readUTF();
             if (response != null && response.equals("OK")) {
                 // Receive and save the file with the same name
                 FileOutputStream fileOutputStream = new FileOutputStream(fileName);
                 byte[] buffer = new byte[1024];
                 int bytesRead;
 
-                while ((bytesRead = socket.getInputStream().read(buffer)) != -1) {
+                while ((bytesRead = dataInputStream.read(buffer)) != -1) {
                     fileOutputStream.write(buffer, 0, bytesRead);
                 }
 
