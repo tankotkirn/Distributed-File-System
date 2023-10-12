ee7bdf3ede6d5c5770672f9194f1ee99a01e430b
Vimoksh Bhavsar <85449611+tankotkirn@users.noreply.github.com>
2023-10-12 12:30:03 +0530

    Update client.java

--- testing/client.java
+++ testing/client.java
@@ -1,31 +1,32 @@
 import java.io.*;
 import java.net.Socket;
 
-public class client {
+public class UpdatedFileClient {
     public static void main(String[] args) {
-        final String SERVER_IP = "10.20.24.70";
-        final int SERVER_PORT = 8080;
+        final String SERVER_IP = "127.0.0.1";
+        final int SERVER_PORT = 12345;
         
         try {
             Socket socket = new Socket(SERVER_IP, SERVER_PORT);
-            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
-            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 
             // Input the file name to request
             System.out.print("Enter the file name to request: ");
             String fileName = new BufferedReader(new InputStreamReader(System.in)).readLine();
 
             // Send the requested file name to the server
-            dataOutputStream.writeUTF(fileName);
+            writer.write(fileName + "\n");
+            writer.flush();
 
-            String response = dataInputStream.readUTF();
+            String response = reader.readLine();
             if (response != null && response.equals("OK")) {
                 // Receive and save the file with the same name
                 FileOutputStream fileOutputStream = new FileOutputStream(fileName);
                 byte[] buffer = new byte[1024];
                 int bytesRead;
 
-                while ((bytesRead = dataInputStream.read(buffer)) != -1) {
+                while ((bytesRead = socket.getInputStream().read(buffer)) != -1) {
                     fileOutputStream.write(buffer, 0, bytesRead);
                 }
 
