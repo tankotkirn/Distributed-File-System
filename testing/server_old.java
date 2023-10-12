29c3091f116cd4f045b05ace7b18a03dd7774ef3
Vimoksh Bhavsar <85449611+tankotkirn@users.noreply.github.com>
2023-10-12 12:21:48 +0530

    Create server_old.java

---
+++ testing/server_old.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class server_old {
+    public static void main(String[] args) {
+        final int PORT = 8080;
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(PORT);
+            System.out.println("Server is listening on port " + PORT);
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Client connected: " + clientSocket.getInetAddress().getHostAddress());
+
+                // Create a new thread to handle the client
+                Thread clientHandler = new Thread(new ClientHandler(clientSocket));
+                clientHandler.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static class ClientHandler implements Runnable {
+        private Socket clientSocket;
+
+        public ClientHandler(Socket socket) {
+            this.clientSocket = socket;
+        }
+
+        @Override
+        public void run() {
+            try {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+
+                String fileName = reader.readLine();
+                System.out.println("Client requested file: " + fileName);
+
+                File file = new File(fileName);
+                if (file.exists()) {
+                    writer.write("OK\n");
+                    writer.flush();
+
+                    FileInputStream fileInputStream = new FileInputStream(file);
+                    byte[] buffer = new byte[1024];
+                    int bytesRead;
+
+                    while ((bytesRead = fileInputStream.read(buffer)) != -1) {
+                        clientSocket.getOutputStream().write(buffer, 0, bytesRead);
+                    }
+
+                    fileInputStream.close();
+                    System.out.println("File sent successfully.");
+                } else {
+                    writer.write("File not found\n");
+                    writer.flush();
+                    System.out.println("File not found.");
+                }
+
+                clientSocket.close();
+                System.out.println("Client disconnected: " + clientSocket.getInetAddress().getHostAddress());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
